a602cb9ed323b3b6d9a00a0eb7490dfe60f1d9cd
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-11-29 11:04:33 +0100

    end of rails wiki

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+resources :articles
+
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/new'
+
+  # get 'articles/create'
+
+  # get 'articles/edit'
+
+  # get 'articles/update'
+
+  # get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
